a7ad94160f54ca3966eebfe1e0580a4efa8e43e6
Sean Tilley <sean@deadsuperhero.com>
2017-05-04 01:36:27 -0700

    fix up some links in views

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -71,6 +71,6 @@ class ArticlesController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def article_params
-      params.require(:article).permit(:title, :body, :published)
+      params.require(:article).permit(:title, :body, :published, :id)
     end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170430070447) do
+ActiveRecord::Schema.define(version: 20170504062831) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -42,6 +42,11 @@ ActiveRecord::Schema.define(version: 20170430070447) do
     t.string   "name"
     t.index ["email"], name: "index_authors_on_email", unique: true, using: :btree
     t.index ["reset_password_token"], name: "index_authors_on_reset_password_token", unique: true, using: :btree
+  end
+
+  create_table "pages", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   add_foreign_key "articles", "authors"
