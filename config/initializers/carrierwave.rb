6a8afc0b52123e11a19e70caffff95ee98ccccaa
Sean Tilley <sean@deadsuperhero.com>
2017-05-07 01:58:50 -0700

    i wonder

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,12 +1,12 @@
 CarrierWave.configure do |config|
 
   config.storage = :aws
-  config.aws_bucket = ENV["AWS_BUCKET"]
+  config.aws_bucket = ENV["S3_BUCKET_NAME"]
   config.aws_acl   = "public-read"
 
   config.aws_credentials = {
-    access_key_id:     ENV["AWS_ACCESS_KEY"],
-    secret_access_key: ENV["AWS_SECRET_KEY"],
+    access_key_id:     ENV["AWS_ACCESS_KEY_ID"],
+    secret_access_key: ENV["AWS_SECRET_ACCESS_KEY"],
     region:            ENV["AWS_REGION"]
 
   }
